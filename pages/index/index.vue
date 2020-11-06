ea83bdcfe2d9adc4e526d893678c4fba0b000374
lovenew57 <2878764693@qq.com>
2020-11-06 15:12:34 +0800

    “swiper3D”

---
+++ pages/index/index.vue
@@ -0,0 +1,68 @@
+<template>
+	<view class="content">
+			<swiper3D  :imgList="imgList" :imgUrl="imgUrl"></swiper3D>
+	</view>
+</template>
+
+<script>
+	import swiper3D from '../../components/swiper3D.vue';
+	export default {
+		data() {
+			return {
+				imgList: [
+					{ src: '../../static/winbox.png' },
+					{ src: '../../static/winbox.png' },
+					{ src: '../../static/winbox.png' },
+					{ src: '../../static/winbox.png' },
+					{ src: '../../static/winbox.png' },
+					{ src: '../../static/winbox.png' },
+				],
+				imgUrl: [
+					{ url: 'other' },
+					{ url: 'other' },
+					{ url: 'other' },
+					{ url: 'other' },
+					{ url: 'other' },
+					{ url: 'other' }
+				],
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		},
+		components: {
+			swiper3D
+		}
+	}
+</script>
+
+<style>
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.logo {
+		height: 200rpx;
+		width: 200rpx;
+		margin-top: 200rpx;
+		margin-left: auto;
+		margin-right: auto;
+		margin-bottom: 50rpx;
+	}
+
+	.text-area {
+		display: flex;
+		justify-content: center;
+	}
+
+	.title {
+		font-size: 36rpx;
+		color: #8f8f94;
+	}
+</style>
